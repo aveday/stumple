57ddad67b49305e8492433522343e5a34c6c4b4c
David Williams <david@aveday.net>
2015-08-20 20:56:03 +1000

    arranged tiles for physics integration

--- engine.cc
+++ engine.cc
@@ -10,19 +10,20 @@ int main(int argc, char *argv[]) {
     
     // Create engine objects
     World *world        = new World();
-    Graphics *graphics  = new Graphics( new Grid(32, LIGHTGREEN, GREEN) );
+    Graphics *graphics  = new Graphics( new Grid(32, 0xff101010, 0xff000000) );
     Control *control    = new Control();
     Clock *clock        = new Clock();
 
 	// Create tiles
     SDL_Texture *t = graphics->GetTexture("assets/brick.png");
-	Tile *tile = new Tile(t, 0, 0, 16, 16);
-	world->AddTile(tile, 1, 1);
-	world->AddTile(tile, 2, 1);
-	world->AddTile(tile, 3, 1);
+	world->AddTile(new Tile(t, 0, 0,  16, 16), 11, 10);
+	world->AddTile(new Tile(t, 0, 16, 16, 16), 12, 10);
+	world->AddTile(new Tile(t, 0, 32, 16, 16), 13, 10);
+	world->AddTile(new Tile(t, 0, 48, 16, 16), 14, 10);
+	world->AddTile(new Tile(t, 0, 64, 16, 16), 15, 10);
 
     // Create game objects
-    Entity *player = new Entity( IVec2(10, 10),
+    Entity *player = new Entity( IVec2(14, 6),
             Image(Polygon(4,16,0), GREY, 0));
 
     world->AddEntity(player);
