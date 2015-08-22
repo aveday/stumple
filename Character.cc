8da30d917c128aa53fddbbd5b314b650aca10b6e
David Williams <david@aveday.net>
2015-08-22 18:27:28 +1000

    completed character skeleton

--- Character.cc
+++ Character.cc
@@ -20,20 +20,46 @@ void Join(World *world,
 
 Character::Character(World *world, Spritesheet *sheet) {
 	// load the sprites
-    Sprite *waist1 =  sheet->GetSprite(0, 0,.4,.4, 1, 1);
-    Sprite *thigh1 =  sheet->GetSprite(0, 1,.3,.8, 1, 1);
-    Sprite *calf1  =  sheet->GetSprite(0, 2,.3,.8, 1, 1);
-    Sprite *shoe1  =  sheet->GetSprite(0, 3,.3,.3, 1, 1);
+    Sprite *head1  = sheet->GetSprite(0, 0,.4,.4, 1, 1);
+    Sprite *torso1 = sheet->GetSprite(0, 1,.5, 1, 1, 1);
+    Sprite *waist1 = sheet->GetSprite(0, 2,.4,.4, 1, 1);
 
-	// create entities with the sprites
-	Entity *waist = world->AddEntity(waist1,7.0f, 6);
+    Sprite *thigh1 = sheet->GetSprite(0, 3,.3,.8, 1, 1);
+    Sprite *calf1  = sheet->GetSprite(0, 4,.3,.8, 1, 1);
+    Sprite *foot1  = sheet->GetSprite(0, 5,.3,.3, 1, 1);
+
+	Sprite *fore1  = sheet->GetSprite(1, 1,.2,.6, 1, 1);
+	Sprite *upper1 = sheet->GetSprite(1, 2,.2,.6, 1, 1);
+	Sprite *hand1  = sheet->GetSprite(1, 3,.2,.2, 1, 1);
+
+
+	// start with the torso
+	Entity *head  = world->AddEntity(head1,  7, 4);
+	Entity *torso = world->AddEntity(torso1, 7, 5);
+	Entity *waist = world->AddEntity(waist1, 7, 6);
 	waist->body->SetFixedRotation(true);
-	Entity *thigh = world->AddEntity(thigh1,7.0f, 7);
-	Entity *calf  = world->AddEntity(calf1, 7.0f, 8);
-	Entity *shoe  = world->AddEntity(shoe1, 7.0f, 9);
+	head->body->SetFixedRotation(true);
 
-	// link up the parts
-	Join(world, waist, b2Vec2(0,0.2), thigh, b2Vec2(0,-0.4), -2*b2_pi/3.0, b2_pi/3.0);
-	Join(world, thigh, b2Vec2(0,0.4), calf,  b2Vec2(0,-0.4), 0.0,        2*b2_pi/3.0);
-	Join(world, calf,  b2Vec2(0,0.4), shoe,  b2Vec2(0,-0.2), -b2_pi/6.0,   b2_pi/3.0);
+	Join(world, head,  b2Vec2(0,0.2), torso, b2Vec2(0,-0.5), -b2_pi/3.0, b2_pi/3.0);
+	Join(world, torso, b2Vec2(0,0.5), waist, b2Vec2(0,0),    -b2_pi/2.0, b2_pi/6.0);
+
+	// then create some legs
+	for(int i = 0; i < 2; i++) {
+		Entity *thigh = world->AddEntity(thigh1,7, 7);
+		Entity *calf  = world->AddEntity(calf1, 7, 8);
+		Entity *foot  = world->AddEntity(foot1, 7, 9);
+		Join(world, waist, b2Vec2(0,0.2), thigh, b2Vec2(0,-0.4), -2*b2_pi/3.0, b2_pi/3.0);
+		Join(world, thigh, b2Vec2(0,0.4), calf,  b2Vec2(0,-0.4), 0.0, 2*b2_pi/3.0);
+		Join(world, calf,  b2Vec2(0,0.4), foot,  b2Vec2(0,-0.2), -b2_pi/6.0,   b2_pi/3.0);
+	}
+
+	// and some arms
+	for(int i = 0; i < 2; i++) {
+		Entity *upper = world->AddEntity(upper1,7, 6);
+		Entity *fore  = world->AddEntity(fore1, 7, 5);
+		Entity *hand  = world->AddEntity(hand1, 7, 7);
+		Join(world, torso, b2Vec2(0,-0.4), upper, b2Vec2(0,-0.3), -b2_pi, b2_pi/3.0);
+		Join(world, upper, b2Vec2(0, 0.3), fore,  b2Vec2(0,-0.3), -5*b2_pi/6, 0);
+		Join(world, fore,  b2Vec2(0, 0.3), hand,  b2Vec2(0,-0.1), -b2_pi/6.0,   b2_pi/3.0);
+	}
 }
