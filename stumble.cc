f2048f1c8e3164044b4e52a4356d28dac665dbad
David Williams <david@aveday.net>
2015-08-24 16:06:38 +1000

    changed main objects to be allocated to stack

--- stumble.cc
+++ stumble.cc
@@ -12,51 +12,48 @@ int main(int argc, char *argv[]) {
         gs = atoi(argv[1]);
     
     // Create engine objects
-    Grid *grid          = new Grid(gs, 0xff101010, 0xff000000);
-    World *world        = new World(b2Vec2(0.0f, 5.0f));
-    Graphics *graphics  = new Graphics(grid);
-    Control *control    = new Control();
-    Clock *clock        = new Clock();
+    Grid grid          = Grid(gs, 0xff101010, 0xff000000);
+    World world        = World(b2Vec2(0.0f, 5.0f));
+    Graphics graphics  = Graphics(&grid);
+    Control control    = Control();
+    Clock clock        = Clock();
 
     // Load textures and create sprites
-    Spritesheet *bricks = graphics->CreateSpritesheet("assets/brick.png", 16, 16, 2, 5);
-    Spritesheet *bricks2= graphics->CreateSpritesheet("assets/brick2.png",16, 16, 4, 4);
-    Spritesheet *rocks  = graphics->CreateSpritesheet("assets/rocks.png", 32, 16, 4, 8);
-    Spritesheet *items  = graphics->CreateSpritesheet("assets/items.png", 16, 16, 4, 4);
-    Spritesheet *tests  = graphics->CreateSpritesheet("assets/test.png",  32, 32, 2, 2);
+    Spritesheet *bricks = graphics.CreateSpritesheet("assets/brick.png", 16, 16, 2, 5);
+    Spritesheet *bricks2= graphics.CreateSpritesheet("assets/brick2.png",16, 16, 4, 4);
+    Spritesheet *rocks  = graphics.CreateSpritesheet("assets/rocks.png", 32, 16, 4, 8);
+    Spritesheet *items  = graphics.CreateSpritesheet("assets/items.png", 16, 16, 4, 4);
+    Spritesheet *tests  = graphics.CreateSpritesheet("assets/test.png",  32, 32, 2, 2);
 	//                                 ords, size, amt
     Sprite *brick = bricks2->GetSprite(0, 0, 1, 1, 4, 4);
     Sprite *crate =  bricks->GetSprite(1, 0, 1, 1, 1, 1);
     Sprite *rock  =   rocks->GetSprite(0, 0, 2, 1, 4, 8);
     Sprite *test  =   tests->GetSprite(0, 0, 1, 1, 1, 1);
 
-	//Character *player = new Character(world, items);
-    //world->characters[world->characterCount++] = player;
+	Character player = Character(&world, items);
+    world.characters[world.characterCount++] = &player;
 
-    int tx = SCR_W/grid->size;
-    int ty = SCR_H/grid->size;
+    int tx = SCR_W/grid.size;
+    int ty = SCR_H/grid.size;
     // Create tiles
     for(int x = 0; x < tx; x++)
         for(int y = ty-4; y < ty; y++)
-            world->AddTile(brick, x, y);
+            world.AddTile(brick, x, y);
 
     // Create some game objects
     for(int y = ty-12; y < ty-5; y++)
-		world->AddEntity(rock, 2, y, 0, 0);
+		world.AddEntity(rock, 2, y, 0, 0);
 
     for(int y = 0; y < 4; y++)
         for(int x = 0; x < tx; x+=4)
-            world->AddEntity(crate, x, y, 0, 0);
+            world.AddEntity(crate, x, y, 0, 0);
 
     // Accept input
-    while(control->GetInput((Character*)NULL)) { // FIXME
-        int t = clock->Sleep();         // Delay to maintain FPS
-                world->Update(t);       // Update the game world
-                graphics->Draw(world);  // Draw to the screen
+    while(control.GetInput((Character*)NULL)) { // FIXME
+        int t = clock.Sleep();         // Delay to maintain FPS
+                world.Update(t);       // Update the game world
+                graphics.Draw(&world);  // Draw to the screen
     }
 
-    // Destroy all the things
-    graphics->~Graphics();
-    
     return 0;
 }
